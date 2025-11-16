efcf5c90b115bd40ce4e9b1c6d1949fc42b1aeb1
Amirul Faudzi <127271948+kinatsuko@users.noreply.github.com>
2025-11-16 17:11:03 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const products = [
+  {
+    category: "Short Sleeve Jersey",
+    items: [
+      { size: "S", quantity: 10, normalPrice: 30, promoPrice: 25 },
+      { size: "M", quantity: 15, normalPrice: 30, promoPrice: 25 }
+    ]
+  }
+];
